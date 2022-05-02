c2570859e49a35b31f9382321c4962b88260cc78
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2022-05-02 02:37:18 -0300

    Coment added.

--- MinMaxSum/Result.cs
+++ MinMaxSum/Result.cs
@@ -5,10 +5,15 @@ namespace MinMaxSum
     public class Result
     {
         /*
-     * Complete the 'miniMaxSum' function below.
-     *
-     * The function accepts INTEGER_ARRAY arr as parameter.
-     */
+         * Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+         * 
+         * Example:
+         * arr= [1,3,5,7,9]
+         * The minimum sum is 1+3+5+7 = 16 and the maximum sum is 3+5+7+9 = 24. 
+         * 
+         * The function prints
+         * 16 24
+         */
 
         public static string minMaxSum(List<int> arr)
         {
