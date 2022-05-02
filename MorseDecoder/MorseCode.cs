7a4f5225262ee1357e636fad1f266faa6e25939e
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2022-05-02 02:24:15 -0300

    Image added.

--- MorseDecoder/MorseCode.cs
+++ MorseDecoder/MorseCode.cs
@@ -4,6 +4,22 @@ namespace MorseDecoder
 {
     public class MorseCode
     {
+        //See the image "Morse Code Translator" that is in the project.
+        //See the image "Morse Code Translator" that is in the project.
+        //See the image "Morse Code Translator" that is in the project.
+
+        /* Background You've started work as morse code translator. Unfortunately some of the signals aren't as distinguishable as others and there are times where a . seems indistinguishable from -. In these cases you write down a ? so that you can figure out what all the posibilities of that letter for that word are later.
+         * Task Write a function possibilities that will take a string signals and return an array of possible characters that the Morse code signals could represent.
+         * 
+         * Specification Challenge.Possibilities(signals) Parameters signals: string - The Morse code signals that needs to be parsed. The may contain one or more unknown signals (?).
+         * Return Value List - The list of possible letters for the given group of signals. Letters will always be ordered from how they appear on the chart, from left to right.
+         * Constraints There will be no more than 3 characters within signals.
+         * 0 - 3 unknown signals may be given. */
+
+        //See the image "Morse Code Translator" that is in the project.
+        //See the image "Morse Code Translator" that is in the project.
+        //See the image "Morse Code Translator" that is in the project.
+
         public static string[] Possibilities(string signals)
         {
             string validatedSignals = SignalsValidate(signals);
