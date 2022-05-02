6daac71035db109a39880c3abc091817ba86c0b4
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2022-05-02 02:11:08 -0300

    MinMaxSum implemented.

---
+++ MinMaxSum/Result.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace MinMaxSum
+{
+    public class Result
+    {
+        /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+        public static string minMaxSum(List<int> arr)
+        {
+            long sumOfAllNumbers = 0L;
+            long minSum = long.MaxValue;
+            long maxSum = 0L;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                sumOfAllNumbers += arr[i];
+
+                if (arr[i] < minSum)
+                    minSum = arr[i];
+
+                if (arr[i] > maxSum)
+                    maxSum = arr[i];
+            }
+
+            return string.Format("{0} {1}", 
+                sumOfAllNumbers - maxSum, sumOfAllNumbers - minSum);
+        }
+    }
+}
