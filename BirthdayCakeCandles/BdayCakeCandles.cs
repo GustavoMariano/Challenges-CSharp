4e76ace38d12d69a05ddecaa03833a33fc2044be
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2022-05-03 00:32:01 -0300

    BdayCakeCandles implemented.

---
+++ BirthdayCakeCandles/BdayCakeCandles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace BirthdayCakeCandles
+{
+    /*
+     * You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+     * Example
+     * candles = [4, 4, 1, 3]
+     * The maximum height candles are 4 units high. There are 2 of them, so return 2.
+     */
+    public class BdayCakeCandles
+    {
+        public static int birthdayCakeCandles(List<int> candles)
+        {
+            candles.Sort();
+            candles.Reverse();
+
+            int higherValue = candles[0];
+            int count = 0;
+
+            foreach (int candle in candles)
+                if (candle == higherValue)
+                    count++;
+
+            return count;
+        }
+    }
+}
---
+++ Tests/BirthdayCakeCandlesTests.cs
@@ -0,0 +1,19 @@
+﻿using BirthdayCakeCandles;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+[TestFixture]
+public class BirthdayCakeCandlesTests
+{
+    List<int> listInt;
+    [Test]
+    public void BasicCases()
+    {
+        listInt = new() { 3, 2, 1, 3 };
+        Assert.AreEqual(2, BdayCakeCandles.birthdayCakeCandles(listInt));
+
+        listInt = new() { 4, 4, 1, 3 };
+        Assert.AreEqual(2, BdayCakeCandles.birthdayCakeCandles(listInt));
+    }
+}
+
