c412fdd52ae7c576be4335ed60fbe9817219405d
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2022-05-04 00:18:00 -0300

    QuotaPerMonth_RafaKowalski implemented.

---
+++ QuotaPerMonth.RafaKowalski/QuotaPerMonth.cs
@@ -0,0 +1,16 @@
+﻿
+namespace QuotaPerMonth_RafaKowalski
+{
+    public class QuotaPerMonth
+    {
+        public double Calculate(int month, double value)
+        {
+            return (value + CalcTax(month, value)) * 1.02;
+        }
+
+        private static double CalcTax(int month, double value)
+        {
+            return value * 0.01 * month;
+        }
+    }
+}
---
+++ Tests/QuotaPerMonthRafaKowalskiTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using QuotaPerMonth_RafaKowalski;
+using System.Collections.Generic;
+
+[TestFixture]
+public class QuotaPerMonthRafaKowalskiTests
+{
+    private QuotaPerMonth quotaPerMonth = new();
+    private int months = 0;
+    private double price = 0;
+
+    [Test]
+    public void BasicCases()
+    {
+        //Arrange
+        months = 3;
+        price = 200;
+        List<double> expectedResult = new() { 206.04, 208.08, 210.12 };
+        List<double> result = new();
+
+        //Action
+        for (int i = 1; i <= months; i++)
+            result.Add(quotaPerMonth.Calculate(i, price));
+
+        //Assert
+        Assert.AreEqual(expectedResult, result);
+    }
+}
