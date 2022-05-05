a58945d6d3290498f91058de3b32da87d678ddc7
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2022-05-05 20:16:53 -0300

    Grading Students implemented.

---
+++ Tests/GradingStudentsTests.cs
@@ -0,0 +1,51 @@
+﻿using GradingStudents;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+[TestFixture]
+public class GradingStudentsTests
+{
+    private List<int> grade;
+
+    [Test]
+    public void CaseOne()
+    {
+        //Arrange
+        grade = new() { 73, 67, 38, 33 };
+        List<int> expectedResult = new() { 75, 67, 40, 33 };
+
+        //Action
+        List<int> result = Solution.Calculate(grade);
+
+        //Assert
+        Assert.AreEqual(expectedResult, result);
+    }
+
+    [Test]
+    public void CaseTwo()
+    {
+        //Arrange
+        grade = new() { 55, 62, 48, 78 };
+        List<int> expectedResult = new() { 55, 62, 50, 80 };
+
+        //Action
+        List<int> result = Solution.Calculate(grade);
+
+        //Assert
+        Assert.AreEqual(expectedResult, result);
+    }
+
+    [Test]
+    public void CaseThree()
+    {
+        //Arrange
+        grade = new() { 8, 43, 77, 99 };
+        List<int> expectedResult = new() { 8, 45, 77, 100 };
+
+        //Action
+        List<int> result = Solution.Calculate(grade);
+
+        //Assert
+        Assert.AreEqual(expectedResult, result);
+    }
+}
