88e8ce61f218295a006eb2525faf3e72fbc76e00
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2022-05-08 23:27:51 -0300

    Odd or Even implemented.

---
+++ OddOrEvenVerificator/OddOrEven.cs
@@ -0,0 +1,18 @@
+﻿
+namespace OddOrEvenVerificator
+{
+    public static class OddOrEven
+    {
+        public static string Verificator(int number)
+        {
+            string result = "";
+
+            if (number % 2 == 0)
+                result = "Even";
+            else
+                result = "Odd";
+
+            return $"{number} is {result}";
+        }
+    }
+}
---
+++ Tests/OddOrEvenTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using OddOrEvenVerificator;
+
+namespace Challenges.Tests
+{
+    [TestFixture]
+    public class OddOrEvenTests
+    {
+        int number = 0;
+
+        [Test]
+        public void ShouldReturn_2_Is_Even()
+        {
+            //Arrange
+            number = 2;
+            string expectedResult = "2 is Even";
+
+            //Action
+            string result = OddOrEven.Verificator(number);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void ShouldReturn_3_Is_Odd()
+        {
+            //Arrange
+            number = 3;
+            string expectedResult = "3 is Odd";
+
+            //Action
+            string result = OddOrEven.Verificator(number);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void ShouldReturn_0_Is_Even()
+        {
+            //Arrange
+            number = 0;
+            string expectedResult = "0 is Even";
+
+            //Action
+            string result = OddOrEven.Verificator(number);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
