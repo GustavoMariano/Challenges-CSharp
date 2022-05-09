80bedee046f398e83384e0ae69e7ec2c8ca8ac33
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2022-05-08 21:48:16 -0300

    Fibonacci implemented.

---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace FibonacciSequence
+{
+    public static class Fibonacci
+    {
+        public static List<int> FibonacciSequenceGenerator(int repetions)
+        {
+            List<int> result = new();
+
+            int x = 1, y = 0, z = 0;
+
+            for (int i = 0; i < repetions; i++)
+            {
+                z = x + y;
+                result.Add(z);
+                x = y;
+                y = z;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tests/FibonacciTests.cs
@@ -0,0 +1,38 @@
+﻿using FibonacciSequence;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Challenges.Tests
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        private int repetions = 0;
+        [Test]
+        public void ShouldReturn_1_1_2()
+        {
+            //Arrange
+            repetions = 3;
+            List<int> expectedResult = new() { 1, 1, 2 };
+
+            //Action
+            List<int> result = Fibonacci.FibonacciSequenceGenerator(repetions);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        public void ShouldReturn_1_1_2_3_5_8_13_21_34()
+        {
+            //Arrange
+            repetions = 9;
+            List<int> expectedResult = new() { 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+
+            //Action
+            List<int> result = Fibonacci.FibonacciSequenceGenerator(repetions);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
