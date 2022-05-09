8ed2a2a79083d95ba34bb162a313272012443fab
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2022-05-08 23:05:40 -0300

    BMI Calculator implemented.

---
+++ BMICalculator/BMI.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+namespace BMICalculator
+{
+    public static class BMI
+    {
+        public static string Calculate(double height, double weight)
+        {
+            double bmi = 0;
+
+            bmi = ((weight / height) / height) * 10000;
+
+            string classificationResult = ClassifyBMI(bmi);
+
+            return $"BMI: {Math.Round(bmi, 1)} - {classificationResult}";
+        }
+
+        private static string ClassifyBMI(double bmi)
+        {
+            string result = "";
+
+            if (bmi < 18.5)
+                result = "Underweight";
+            else if (bmi >= 18.5 && bmi < 25)
+                result = "Normal weight";
+            else if (bmi >= 25 && bmi < 30)
+                result = "Overweight";
+            else
+                result = "Obesity";
+
+            return result;
+        }
+    }
+}
---
+++ Tests/BMICalculatorTests.cs
@@ -0,0 +1,57 @@
+﻿using BMICalculator;
+using NUnit.Framework;
+
+namespace Challenges.Tests
+{
+    [TestFixture]
+    public class BMICalculatorTests
+    {
+        private double height = 0; //centimeters
+        private double weight = 0; //kilograms
+
+        [Test]
+        public void ShouldReturn_22_2_Normal_weight()
+        {
+            //Arrange
+            height = 190;
+            weight = 80;
+            string expectedResult = "BMI: 22.2 - Normal weight";
+
+            //Action
+            string result = BMI.Calculate(height, weight);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void ShouldReturn_17_3_Underweight()
+        {
+            //Arrange
+            height = 170;
+            weight = 50;
+            string expectedResult = "BMI: 17.3 - Underweight";
+
+            //Action
+            string result = BMI.Calculate(height, weight);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void ShouldReturn_31_Obesity()
+        {
+            //Arrange
+            height = 175;
+            weight = 95;
+            string expectedResult = "BMI: 31 - Obesity";
+
+            //Action
+            string result = BMI.Calculate(height, weight);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
