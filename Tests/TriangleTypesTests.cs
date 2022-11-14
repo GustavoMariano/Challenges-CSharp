f4592561bf3ccdd012d51e55b94dddb48c3bf7a7
Gustavo Cesar Mariano <gustavo.mariano@kinvo.com.br>
2022-11-14 11:21:53 -0300

    TriangleTypes implemented.

---
+++ Tests/TriangleTypesTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using TriangleTypes;
+
+namespace Challenges.Tests
+{
+    [TestFixture]
+    public class TriangleTypesTests
+    {
+        [Test]
+        public void DeveSerTrianguloEquilatero()
+        {
+            Triangle equilateral = new(3, 3, 3);
+
+            string result = equilateral.Validate();
+
+            Assert.AreEqual("Equilateral Triangle", result);
+        }
+
+        [Test]
+        public void DeveSerTrianguloIsoceles()
+        {
+            Triangle isosceles = new(3, 3, 1);
+
+            string result = isosceles.Validate();
+
+            Assert.AreEqual("Isosceles Triangle", result);
+        }
+
+        [Test]
+        public void DeveSerTrianguloEscaleno()
+        {
+            Triangle scalene = new(4, 3, 2);
+
+            string result = scalene.Validate();
+
+            Assert.AreEqual("Scalene Triangle", result);
+        }
+
+
+        [Test]
+        public void DeveSerTrianguloInvalido()
+        {
+            Triangle invalid = new(3, 2, 1);
+
+            string result = invalid.Validate();
+
+            Assert.AreEqual("Invalid Triangle", result);
+        }
+    }
+}
---
+++ TriangleTypes/Triangle.cs
@@ -0,0 +1,44 @@
+﻿namespace TriangleTypes
+{
+    public class Triangle
+    {
+        private double SideA = 0, SideB = 0, SideC = 0;
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        public string Validate()
+        {
+            if (IsValid())
+            {
+                if (IsEquilateral())
+                    return "Equilateral Triangle";
+                else if (IsIsosceles())
+                    return "Isosceles Triangle";
+                else
+                    return "Scalene Triangle";
+            }
+            else
+                return "Invalid Triangle";
+        }
+
+        private bool IsEquilateral()
+        {
+            return SideA == SideB && SideA == SideC;
+        }
+
+        private bool IsIsosceles()
+        {
+            return SideA == SideB || SideA == SideC || SideB == SideC;
+        }
+
+        private bool IsValid()
+        {
+            return SideA + SideB > SideC && SideA + SideC > SideB && SideB + SideC > SideA;
+        }
+    }
+}
