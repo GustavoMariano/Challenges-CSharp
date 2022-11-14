f979b27f2084a23651f5ed2f1b902e0be3c9175c
Gustavo Cesar Mariano <gustavo.mariano@kinvo.com.br>
2022-11-14 16:43:58 -0300

    ManualDateValidation implemented.

---
+++ ManualDateValidation/DateValidation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace ManualDateValidation
+{
+    public class DateValidation
+    {
+        public void IntToDate(int day, int month, int year)
+        {
+            DateTime dataConvertida;
+
+            if (year > 0 && year <= 3000) //Fictitious limit
+            {
+                if (Have31Days(month))
+                {
+                    if (day < 1 || day > 31)
+                        throw new ArgumentException("Day informed is invalid!!");
+                    else
+                        dataConvertida = new DateTime(year, month, day);
+                }
+                else if (Have30Days(month))
+                {
+                    if (day < 1 || day > 30)
+                        throw new ArgumentException("Day informed is invalid!!");
+                    else
+                        dataConvertida = new DateTime(year, month, day);
+                }
+                else if (IsFebruary(month))
+                {
+                    if (!DateTime.IsLeapYear(year))
+                    {
+                        if (day < 1 || day > 28)
+                            throw new ArgumentException("The year informed is not a leap year, so February has 28 days!!");
+                        else
+                            dataConvertida = new DateTime(year, month, day);
+                    }
+                    else
+                    {
+                        if (day < 1 || day > 29)
+                            throw new ArgumentException("The year informed is a leap year, however the day informed is not valid!!");
+                        else
+                            dataConvertida = new DateTime(year, month, day);
+                    }
+                }
+                else
+                    throw new ArgumentException("The month entered is invalid!!");
+            }
+            else
+                throw new ArgumentException("Entered year is out of validation scope(0001 ~3000)");
+        }        
+
+        public void StringToDate(string day, string month, string year)
+        {
+            int intDay;
+            int intMonth; //= Convert.ToInt32(month);
+            int intYear; //= Convert.ToInt32(year);
+
+            if (!Int32.TryParse(day, out intDay))
+                throw new ArgumentException("The day entered is invalid!!");
+
+            if (!Int32.TryParse(month, out intMonth))
+                throw new ArgumentException("The month entered is invalid!!");
+
+            if (!Int32.TryParse(year, out intYear))
+                throw new ArgumentException("The year entered is invalid!!");
+
+            //DateTime dataConvertida;
+
+            IntToDate(intDay, intMonth, intYear);
+        }
+
+        #region If conditions
+        private static bool IsFebruary(int month)
+        {
+            return month == 2;
+        }
+        private static bool Have30Days(int month)
+        {
+            return month == 4 || month == 6 || month == 9 || month == 11;
+        }
+        private static bool Have31Days(int month)
+        {
+            return month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12;
+        }
+        #endregion
+    }
+}
---
+++ Tests/ManualDateValidationTests.cs
@@ -0,0 +1,144 @@
+﻿using ManualDateValidation;
+using NUnit.Framework;
+using System;
+
+namespace Challenges.Tests
+{
+    [TestFixture]
+    public class ManualDateValidationTests
+    {
+        DateValidation dateValidation;
+
+        public ManualDateValidationTests()
+        {
+            dateValidation = new();
+        }
+
+        #region Year Validation
+        [Test]
+        public void YearBelow1_Should_ThrowException()
+        {
+            //Arrange / Action
+            int day = 01, month = 01, year = 0;
+
+            //Assert
+            Assert.That(() => dateValidation.IntToDate(day, month, year),
+                Throws.TypeOf<ArgumentException>().With.Message.EqualTo("Entered year is out of validation scope(0001 ~3000)"));
+        }
+
+        [Test]
+        public void YearAbove3000_Should_ThrowException()
+        {
+            //Arrange / Action
+            int day = 01, month = 01, year = 3001;
+
+            //Assert
+            Assert.That(() => dateValidation.IntToDate(day, month, year),
+                Throws.TypeOf<ArgumentException>().With.Message.EqualTo("Entered year is out of validation scope(0001 ~3000)"));
+        }
+
+        [Test]
+        public void YearValid_ShouldNot_ThrowException()
+        {
+            //Arrange / Action
+            int day = 01, month = 01, year = 2000;
+
+            //Assert
+            Assert.DoesNotThrow(() => dateValidation.IntToDate(day, month, year));
+        }
+        #endregion
+
+        #region Month Validation
+        [Test]
+        public void MonthBelow1_Should_ThrowException()
+        {
+            int day = 01, month = 00, year = 2021;
+
+            Assert.That(() => dateValidation.IntToDate(day, month, year),
+                Throws.TypeOf<ArgumentException>().With.Message.EqualTo("The month entered is invalid!!"));
+        }
+
+        [Test]        
+        public void MonthAbove12_Should_ThrowException()
+        {
+            int day = 01, month = 13, year = 2021;
+
+            Assert.That(() => dateValidation.IntToDate(day, month, year),
+                Throws.TypeOf<ArgumentException>().With.Message.EqualTo("The month entered is invalid!!"));
+        }
+
+        [Test]
+        public void MonthValid_ShouldNot_ThrowException()
+        {
+            int day = 01, month = 01, year = 2021;
+
+            Assert.DoesNotThrow(() => dateValidation.IntToDate(day, month, year));
+        }
+        #endregion
+
+        #region Day Validation
+        [Test]
+        public void DayBelow1_Should_ThrowException()
+        {
+            int day = 00, month = 01, year = 2021;
+
+            Assert.That(() => dateValidation.IntToDate(day, month, year),
+                Throws.TypeOf<ArgumentException>().With.Message.EqualTo("Day informed is invalid!!"));
+        }
+
+        #region 31 day month test cases
+        [Test]
+        [TestCase(01)]
+        [TestCase(03)]
+        [TestCase(05)]
+        [TestCase(07)]
+        [TestCase(08)]
+        [TestCase(10)]
+        [TestCase(12)]
+        public void MonthAbove31_Should_ThrowException(int month)
+        {
+            int day = 32, year = 2020;
+
+            Assert.That(() => dateValidation.IntToDate(day, month, year),
+                Throws.TypeOf<ArgumentException>().With.Message.EqualTo("Day informed is invalid!!"));
+        }
+        #endregion
+
+        #region 30 day month test cases
+        [Test]
+        [TestCase(04)]
+        [TestCase(06)]
+        [TestCase(09)]
+        [TestCase(11)]
+        public void DayAbove30_Should_ThrowException(int month)
+        {
+            int day = 31, year = 2021;
+
+            Assert.That(() => dateValidation.IntToDate(day, month, year),
+                Throws.TypeOf<ArgumentException>().With.Message.EqualTo("Day informed is invalid!!"));
+        }
+        #endregion
+
+        #region February Validation
+        [Test]
+        public void Day29OrAbove_InFebruary_IsNotLeapYear_Should_ThrowException()
+        {
+            int day = 29, month = 02, year = 2021;
+
+            Assert.That(() => dateValidation.IntToDate(day, month, year),
+                Throws.TypeOf<ArgumentException>().With.Message.EqualTo("The year informed is not a leap year, so February has 28 days!!"));
+        }
+
+        [Test]
+        public void DayAbove29_InFebruary_IsLeapYear_Should_ThrowException()
+        {
+            int day = 30, month = 02, year = 2020;
+
+            Assert.That(() => dateValidation.IntToDate(day, month, year),
+                Throws.TypeOf<ArgumentException>().With.Message.EqualTo("The year informed is a leap year, however the day informed is not valid!!"));
+        }
+        #endregion
+
+        #endregion
+    }
+}
