8533a5394a6fbbe3bfbb7a235658320b332e18c6
Gustavo Mariano <gustavocmariano@hotmail.com>
2023-07-31 15:05:30 -0300

    SolarPoweredBulbs_AWS_Exam implemented.

---
+++ AWS-Test/SolarPoweredBulbs_AWS_Exam.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AWS_Test_HackerRank
+{
+    public class SolarPoweredBulbs_AWS_Exam
+    {
+
+        /*
+         * Complete the 'maximizeSolarPoweredBulbs' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts STRING bulbs as parameter.
+         */
+
+        public static int MaximizeSolarPoweredBulbs(string bulbs)
+        {
+            if (string.IsNullOrEmpty(bulbs))
+                return 0;
+
+            if (bulbs.Length == 1)
+                return 1;
+
+            int total = 0;
+            int countZero = 0;
+            int anterior = 0;
+            List<int> bulbsList = new List<int>();
+
+            foreach (char number in bulbs)
+                bulbsList.Add((int)Char.GetNumericValue(number));
+
+            for (int i = 0; i < bulbsList.Count; i++)
+            {
+                anterior = i - 1;
+
+                if (i == 0 && bulbsList[0] == 0 && bulbsList[1] == 0)
+                {
+                    countZero = 0;
+                    bulbsList[i] = 1;
+                }
+
+                if (bulbsList[i] == 0)
+                {
+                    countZero++;
+
+                    if (countZero == 3)
+                    {
+                        countZero = 1;
+                        bulbsList[anterior] = 1;
+                    }
+                    if (bulbsList[i] == 0 && i == bulbsList.Count - 1 && bulbsList[anterior] == 0 && countZero == 2)
+                        bulbsList[i] = 1;
+                }
+                else if (bulbsList[i] == 1)
+                        countZero = 0;
+            }
+
+            foreach (int number in bulbsList)
+                total += number;
+
+            return total;
+        }
+    }
+}
---
+++ Tests/AWSTest-Tests.cs
@@ -0,0 +1,82 @@
+﻿using AWS_Test_HackerRank;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Challenges.Tests
+{
+    [TestFixture]
+    public class AWS_Test_HackerRankTests
+    {
+        private int expectedResult = 1;
+
+        [Test]
+        public void ShouldReturn_One_OneSolar()
+        {
+            //Arrange
+            expectedResult = 1;
+            string input = "1";
+
+            //Action
+            int result = SolarPoweredBulbs_AWS_Exam.MaximizeSolarPoweredBulbs(input);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void ShouldReturn_One_ZeroSolar()
+        {
+            //Arrange
+            expectedResult = 1;
+            string input = "0";
+
+            //Action
+            int result = SolarPoweredBulbs_AWS_Exam.MaximizeSolarPoweredBulbs(input);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void ShouldReturn_Six_ThreeSolar()
+        {
+            //Arrange
+            expectedResult = 6;
+            string input = "00101000100";
+
+            //Action
+            int result = SolarPoweredBulbs_AWS_Exam.MaximizeSolarPoweredBulbs(input);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void ShouldReturn_TwentySix_TwentySolar()
+        {
+            //Arrange
+            expectedResult = 26;
+            string input = "0010100010001010100101000100100100100101010101010001100";
+
+            //Action
+            int result = SolarPoweredBulbs_AWS_Exam.MaximizeSolarPoweredBulbs(input);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void ShouldReturn_Six_ZeroSolar()
+        {
+            //Arrange
+            expectedResult = 6;
+            string input = "00000000000";
+
+            //Action
+            int result = SolarPoweredBulbs_AWS_Exam.MaximizeSolarPoweredBulbs(input);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
