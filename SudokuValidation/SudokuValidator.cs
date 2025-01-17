29e494a8e1439885a0cc36f8ca06ff046268a331
Gustavo Mariano <gustavocmariano@hotmail.com>
2025-01-17 00:10:55 -0300

    Sudoku validator implemented

---
+++ SudokuValidation/SudokuValidator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace SudokuValidation
+{
+
+    /*
+     *Each row contains unique values from 1 to 9 (when filled).
+     *Each column contains unique values from 1 to 9 (when filled).
+     *Each 3x3 region contains unique values from 1 to 9 (when filled).
+     *
+     *Note: This applies to the current state. 
+     */
+
+    public class SudokuValidator
+    {
+        public static bool Validator(List<List<string>> sudoku)
+        {
+            HashSet<string>[] rows = new HashSet<string>[9];
+            HashSet<string>[] cols = new HashSet<string>[9];
+            HashSet<string>[] boxes = new HashSet<string>[9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                rows[i] = new HashSet<string>();
+                cols[i] = new HashSet<string>();
+                boxes[i] = new HashSet<string>();
+            }
+
+            for (int r = 0; sudoku.Count > r; r++)
+                for (int c = 0; sudoku.Count > c; c++)
+                {
+                    string current = sudoku[r][c];
+
+                    if (current == "")
+                        continue;
+
+                    int boxIndex = (r / 3) * 3 + c / 3;
+
+                    if (rows[r].Contains(current) || cols[c].Contains(current) || boxes[boxIndex].Contains(current))
+                        return false;
+
+                    rows[r].Add(current);
+                    cols[c].Add(current);
+                    boxes[boxIndex].Add(current);
+                }
+
+            return true;
+        }
+    }
+}
---
+++ Tests/SudokuValidationTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using SudokuValidation;
+using System.Collections.Generic;
+
+namespace Challenges.Tests
+{
+    [TestFixture]
+    public class SudokuValidationTests
+    {
+        [Test]
+        public void ShouldReturnTrue()
+        {
+            List<List<string>> sudoku = new()
+            {
+                new List<string> { "5", "3", "", "", "7", "", "", "", "" },
+                new List<string> { "6", "", "", "1", "9", "5", "", "", "" },
+                new List<string> { "", "9", "8", "", "", "", "", "6", "" },
+                new List<string> { "8", "", "", "", "6", "", "", "", "3" },
+                new List<string> { "4", "", "", "8", "", "3", "", "", "1" },
+                new List<string> { "7", "", "", "", "2", "", "", "", "6" },
+                new List<string> { "", "6", "", "", "", "", "2", "8", "" },
+                new List<string> { "", "", "", "4", "1", "9", "", "", "5" },
+                new List<string> { "", "", "", "", "8", "", "", "7", "9" }
+            };
+
+            Assert.AreEqual(true, SudokuValidator.Validator(sudoku));
+        }
+
+        [Test]
+        public void ShouldReturnFalse()
+        {
+            List<List<string>> sudoku = new()
+            {
+                new List<string> { "5", "3", "", "", "7", "", "", "", "" },
+                new List<string> { "6", "", "", "1", "9", "5", "", "", "" },
+                new List<string> { "", "9", "8", "", "", "", "", "6", "" },
+                new List<string> { "8", "", "", "", "6", "", "", "", "3" },
+                new List<string> { "4", "", "", "8", "", "3", "", "", "1" },
+                new List<string> { "7", "", "", "", "2", "", "", "", "6" },
+                new List<string> { "", "6", "", "", "", "", "2", "8", "" },
+                new List<string> { "", "", "", "4", "1", "9", "", "", "5" },
+                new List<string> { "", "", "", "", "8", "9", "", "7", "9" }
+            };
+
+            Assert.AreEqual(false, SudokuValidator.Validator(sudoku));
+        }
+    }
+}
