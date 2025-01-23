f12d16205d9f7410393fce12b9d6103cba4cf9ac
Gustavo Mariano <gustavocmariano@hotmail.com>
2025-01-23 12:46:58 -0300

    Added NumberAsArraySum

---
+++ NumberAsArraySum/NumberAsArray.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace NumberAsArraySum
+{
+    public class NumberAsArray
+    {
+        public static int[] Increment(int[] numbers)
+        {
+            int number = Convert.ToInt32(string.Join("", numbers)) + 1;
+
+            return number.ToString().Select(x => int.Parse(x.ToString())).ToArray();
+        }
+    }
+}
---
+++ Tests/NumberAsArraySumTests.cs
@@ -0,0 +1,37 @@
+﻿using NumberAsArraySum;
+using NUnit.Framework;
+
+namespace Challenges.Tests
+{
+    [TestFixture]
+    public class NumberAsArraySumTests
+    {
+        [Test]
+        public void Test1()
+        {
+            //Arrange
+            int[] inputNumbers = new[] { 1, 2, 3};
+            int[] expected = new[] { 1, 2, 4 };
+
+            //Action
+            int[] result = NumberAsArray.Increment(inputNumbers);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            //Arrange
+            int[] inputNumbers = new[] { 9, 9, 9 };
+            int[] expected = new[] { 1, 0, 0, 0 };
+
+            //Action
+            int[] result = NumberAsArray.Increment(inputNumbers);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
