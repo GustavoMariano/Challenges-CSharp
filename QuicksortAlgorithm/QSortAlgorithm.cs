f13d5e2614a1210215b42623c4402dcb662b2661
Gustavo Mariano <gustavocmariano@hotmail.com>
2025-02-07 00:15:57 -0300

    Quicksort implemented

---
+++ QuicksortAlgorithm/QSortAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace QuicksortAlgorithm
+{
+    public class QSortAlgorithm
+    {
+        public static int[] Quicksort(int[] nums)
+        {
+            if (nums.Length < 2)
+                return nums;
+            else
+            {
+                int pivot = nums[0];
+                int[] minors = nums.Skip(1).Where(i => i <= pivot).ToArray();
+                int[] majors = nums.Skip(1).Where(i => i > pivot).ToArray();
+
+                return Quicksort(minors).Concat(new int[] { pivot }).Concat(Quicksort(majors)).ToArray();
+            }
+        }
+    }
+}
---
+++ Tests/QuicksortAlgorithmTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using QuicksortAlgorithm;
+
+namespace Challenges.Tests
+{
+    [TestFixture]
+    public class QuicksortAlgorithmTests
+    {
+        [Test]
+        public void ShouldReturn_Array_FourNumbers()
+        {
+            //Arrange
+            int[] array = { 3, 2, 4, 1 };
+            int[] expectedResult = { 1, 2, 3, 4 };
+
+            //Action
+            var result = QSortAlgorithm.Quicksort(array);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void ShouldReturn_Array_TenNumbers()
+        {
+            //Arrange
+            int[] array = { 527, 184, 693, 872, 305, 941, 76, 458, 612, 389 };
+            int[] expectedResult = { 76, 184, 305, 389, 458, 527, 612, 693, 872, 941 };
+
+            //Action
+            var result = QSortAlgorithm.Quicksort(array);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
