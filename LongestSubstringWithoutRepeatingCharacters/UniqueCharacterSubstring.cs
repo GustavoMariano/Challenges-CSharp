79da9aec6caf908cc307235dc146002de8574f25
Gustavo Mariano <gustavocmariano@hotmail.com>
2025-02-08 05:38:03 -0300

    LongestSubstringWithoutRepeatingCharacters added.

---
+++ LongestSubstringWithoutRepeatingCharacters/UniqueCharacterSubstring.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace LongestSubstringWithoutRepeatingCharacters
+{
+    public class UniqueCharacterSubstring
+    {
+        public static int Solution(string str) 
+        {
+            if (string.IsNullOrEmpty(str))
+                return 0;
+
+            int longestSubstring = 0;
+            int leftIndex = 0;
+            var charSet = new HashSet<char>();
+
+            for (int rightIndex = 0; rightIndex < str.Length; rightIndex++)
+            {
+                while (charSet.Contains(str[rightIndex]))
+                {
+                    charSet.Remove(str[leftIndex]);
+                    leftIndex++;
+                }
+
+                charSet.Add(str[rightIndex]);
+
+                longestSubstring = Math.Max(longestSubstring, rightIndex - leftIndex + 1);
+            }
+
+            return longestSubstring;
+        }
+    }
+}
---
+++ Tests/LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,100 @@
+﻿using LongestSubstringWithoutRepeatingCharacters;
+using NUnit.Framework;
+
+namespace Challenges.Tests
+{
+    [TestFixture]
+    public class LongestSubstringWithoutRepeatingCharactersTests
+    {
+        [Test]
+        public void ShouldReturn_3_abcabcbb()
+        {
+            //Arrange
+            string str = "abcabcbb";
+
+            //Action
+            int result = UniqueCharacterSubstring.Solution(str);
+
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void ShouldReturn_1_bbbbb()
+        {
+            //Arrange
+            string str = "bbbbb";
+
+            //Action
+            int result = UniqueCharacterSubstring.Solution(str);
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void ShouldReturn_3_pwwkew()
+        {
+            //Arrange
+            string str = "pwwkew";
+
+            //Action
+            int result = UniqueCharacterSubstring.Solution(str);
+
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void ShouldReturn_0()
+        {
+            //Arrange
+            string str = "";
+
+            //Action
+            int result = UniqueCharacterSubstring.Solution(str);
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void ShouldReturn_1_()
+        {
+            //Arrange
+            string str = " ";
+
+            //Action
+            int result = UniqueCharacterSubstring.Solution(str);
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void ShouldReturn_2_au()
+        {
+            //Arrange
+            string str = "au";
+
+            //Action
+            int result = UniqueCharacterSubstring.Solution(str);
+
+            //Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void ShouldReturn_3_dvdf()
+        {
+            //Arrange
+            string str = "dvdf";
+
+            //Action
+            int result = UniqueCharacterSubstring.Solution(str);
+
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+    }
+}
