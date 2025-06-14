8a4d3ef7fd818a49752f0c412c19c90556fd4283
Gustavo Mariano <gustavocmariano@hotmail.com>
2025-06-14 03:26:23 -0300

    SOTWA

---
+++ SumOfTwoArrays/SumTwoArrays.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace SumOfTwoArrays
+{
+    public class SumTwoArrays
+    {
+        public static int[] Sum(int[] num1, int[] num2)
+        {
+            string number1 = string.Join("", num1);
+            string number2 = string.Join("", num2);
+
+            int sum = int.Parse(number1) + int.Parse(number2);
+
+            return sum.ToString().Select(c => c - '0').ToArray();
+        }
+    }
+}
---
+++ Tests/SumOfTwoArraysTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using SumOfTwoArrays;
+
+namespace Challenges.Tests
+{
+    [TestFixture]
+    public class SumOfTwoArraysTests
+    {
+        [Test]
+        public void ShouldReturn199()
+        {
+            //Arrange
+            int[] arrayNumber1 = { 1, 0, 1};
+            int[] arrayNumber2 = { 9, 8};
+            int[] expectedResult = { 1, 9, 9};
+
+            //Action
+            int[] result = SumTwoArrays.Sum(arrayNumber1, arrayNumber2);
+            
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+
+        [Test]
+        public void ShouldReturn52314()
+        {
+            //Arrange
+            int[] arrayNumber1 = { 2, 6, 0, 0, 0 };
+            int[] arrayNumber2 = { 2, 6, 3, 1, 4 };
+
+            //Action
+            int[] expectedResult = { 5, 2, 3, 1, 4 };
+            int[] result = SumTwoArrays.Sum(arrayNumber1, arrayNumber2);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
