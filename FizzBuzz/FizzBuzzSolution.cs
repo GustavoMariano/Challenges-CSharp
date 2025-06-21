428ddabc08935ec20020b77df0898f8347f8d884
Gustavo Mariano <gustavocmariano@hotmail.com>
2025-06-21 04:38:08 -0300

    FizzBuzz added.

---
+++ FizzBuzz/FizzBuzzSolution.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzSolution
+    {
+        public static IList<string> Solution(int n)
+        {
+            string[] result = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = i + 1;
+                if (x % 3 == 0 && x % 5 == 0)
+                    result[i] = "FizzBuzz";
+                else if (x % 3 == 0)
+                    result[i] = "Fizz";
+                else if (x % 5 == 0)
+                    result[i] = "Buzz";
+                else
+                    result[i] = x.ToString();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tests/FizzBuzzTests.cs
@@ -0,0 +1,52 @@
+﻿using FizzBuzz;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Challenges.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void FizzBuzz_Input3()
+        {
+            //Arrange
+            int n = 3;
+            string[] expectedResult = new string[] { "1", "2", "Fizz" };
+
+            //Action
+            IList<string> result = FizzBuzzSolution.Solution(n);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void FizzBuzz_Input5()
+        {
+            //Arrange
+            int n = 5;
+            string[] expectedResult = new string[] { "1", "2", "Fizz", "4", "Buzz" };
+
+            //Action
+            IList<string> result = FizzBuzzSolution.Solution(n);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void FizzBuzz_Input15()
+        {
+            //Arrange
+            int n = 15;
+            string[] expectedResult = new string[] { "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz" };
+
+            //Action
+            IList<string> result = FizzBuzzSolution.Solution(n);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
