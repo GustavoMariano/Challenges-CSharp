2a11902f34c8cd2b942367b047f6104505e1b061
Gustavo Mariano <gustavocmariano@hotmail.com>
2025-06-21 04:22:02 -0300

    GrayCode added

---
+++ GrayCode/GrayCodeSequence.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace GrayCode
+{
+    public class GrayCodeSequence
+    {
+        public static IList<int> Solution(int n)
+        {
+            int possibleGrayCodes = (1 << n);
+            int[] result = new int[possibleGrayCodes];
+
+            for (int i = 0; i < possibleGrayCodes; i++)
+                result[i] = (i ^ (i >> 1));
+
+            return result;
+        }
+    }
+}
---
+++ Tests/GrayCodeTests.cs
@@ -0,0 +1,52 @@
+﻿using GrayCode;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Challenges.Tests
+{
+    [TestFixture]
+    public class GrayCodeTests
+    {
+        [Test]
+        public void CaseOne()
+        {
+            //Arrange
+            int n = 2;
+            List<int> expectedResult = new() { 0, 1, 3, 2 };
+
+            //Action
+            IList<int> result = GrayCodeSequence.Solution(n);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void CaseTwo()
+        {
+            //Arrange
+            int n = 3;
+            List<int> expectedResult = new() { 0, 1, 3, 2, 6, 7, 5, 4 };
+
+            //Action
+            IList<int> result = GrayCodeSequence.Solution(n);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void CaseThree()
+        {
+            //Arrange
+            int n = 1;
+            List<int> expectedResult = new() { 0, 1 };
+
+            //Action
+            IList<int> result = GrayCodeSequence.Solution(n);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
