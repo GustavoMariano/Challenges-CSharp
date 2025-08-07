f671be0ec5574c51b0a6fa5b008bc25685a81f04
Gustavo Mariano <gustavocmariano@hotmail.com>
2025-08-06 23:37:06 -0300

    CountingRepeatedNumbers implemented.

---
+++ CountingRepeatedNumbersInAList/CountingRepeatedNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace CountingRepeatedNumbersInAList
+{
+    public class CountingRepeatedNumbers
+    {
+        public static int CountRepeatedNumbers(List<int> numbers)
+        {
+            int count = 0;
+
+            Dictionary<int, int> numberFrequency = new Dictionary<int, int>();
+
+            foreach (var item in numbers)
+            {
+                if (numberFrequency.ContainsKey(item))
+                {
+                    count++;
+                    numberFrequency[item]++;
+                }
+                else
+                    numberFrequency[item] = 1;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Tests/CountingRepeatedNumbersInAListTests.cs
@@ -0,0 +1,24 @@
+﻿using CountingRepeatedNumbersInAList;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Challenges.Tests
+{
+    [TestFixture]
+    public class CountingRepeatedNumbersInAListTests
+    {
+        [Test]
+        public void FizzBuzz_Input3()
+        {
+            //Arrange
+            List<int> inputIntList= new () { 4, 17, 23, 4, 89, 23, 50, 61, 77, 4, 17, 90, 11, 12, 13, 50, 61, 70, 80, 90 };
+            int expectedResult = 6;
+
+            //Action
+            int result = CountingRepeatedNumbers.CountRepeatedNumbers(inputIntList);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
